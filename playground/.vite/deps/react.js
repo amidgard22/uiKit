369c0a73ba04b76b5badf004b1c4b828b7ab31cf
amidgard <midgardalex22@gmail.com>
2026-06-12 21:20:29 +0300

    create arhitecture and first 2 components

---
+++ playground/.vite/deps/react.js
@@ -0,0 +1,4 @@
+import {
+  require_react
+} from "./chunk-TFY6DHY4.js";
+export default require_react();
--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import '../../src/styles/globals.css';
+
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
